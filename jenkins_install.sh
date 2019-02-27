f077c578b52f5c523cb6b3f30c2d9e469b3c06bd
William Zhou <williamkzhou@hotmail.com>
2019-02-27 20:05:38 +0000

    Update jenkins_install.sh

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,3 +1,10 @@
+# Jenkins wiki on installing Jenkins in a CentOS environment: 
+# https://wiki.jenkins.io/display/JENKINS/Installing+Jenkins+on+Red+Hat+distributions
+
+# Jenkins installation docs: 
+# https://jenkins.io/doc/book/installing/
+
+
 sudo yum -y remove java
 sudo yum -y install java-1.8.0-openjdk
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
