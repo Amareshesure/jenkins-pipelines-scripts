6972750bd20cf126904873f78c97d1ddbcbc32bb
William Zhou <williamkzhou@hotmail.com>
2019-02-27 12:51:10 +0000

    Update spring-petclinic-pipeline.groovy

--- spring-petclinic-pipeline.groovy
+++ spring-petclinic-pipeline.groovy
@@ -3,28 +3,23 @@ node
    try
    {
        notify('Started')
-       stage('Preparation') 
+       stage('Checkout Git') 
        { // for display purposes
           // Get some code from a GitHub repository
-          git 'https://github.com/g0t4/jenkins2-course-spring-petclinic.git'
-          
+          git 'https://github.com/g0t4/jenkins2-course-spring-boot.git'
        }
        stage('Build')
        {
-            sh 'mvn clean package'
-       }
-       stage('Generate Archive') 
-       {
-          archiveArtifacts "target/*.?ar"
+            def project_path = "spring-boot-samples/spring-boot-sample-atmosphere"
+            dir(project_path)
+            {
+                sh 'mvn clean package'
+                archiveArtifacts "target/*.?ar"
+            }
        }
        stage('Test Results')
        {
            step([$class: 'JUnitResultArchiver', testResults: 'target/surefire-reports/TEST-*.xml'])
-       }
-       stage('Results') 
-       {
-          step([$class: 'JUnitResultArchiver', testResults: 'target/surefire-reports/TEST-*.xml'])
-          archiveArtifacts "target/*.?ar"
        }
        stage('Email Build Output')
        {
@@ -44,6 +39,8 @@ def notify(status)
       to: "williamkzhou@hotmail.com",
       subject: "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
       body: """<p>${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-        <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""",
+      <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""",
     )
 }
+
+
