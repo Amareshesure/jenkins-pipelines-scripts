4fb8a5badadca3e5818b3d2fa414bc95cd6a76aa
William Zhou <williamkzhou@hotmail.com>
2019-02-27 19:13:52 +0000

    Create solitaire-pipeline.groovy

---
+++ solitaire-pipeline.groovy
@@ -0,0 +1,72 @@
+stage 'CI'
+node {
+
+    git branch: 'jenkins2-course', 
+        url: 'https://github.com/g0t4/solitaire-systemjs-course'
+
+    // pull dependencies from npm
+    // on windows use: bat 'npm install'
+    sh 'npm install'
+
+    // stash code & dependencies to expedite subsequent testing
+    // and ensure same code & dependencies are used throughout the pipeline
+    // stash is a temporary archive
+    stash name: 'everything', 
+          excludes: 'test-results/**', 
+          includes: '**'
+    
+    // test with PhantomJS for "fast" "generic" results
+    // on windows use: bat 'npm run test-single-run -- --browsers PhantomJS'
+    sh 'npm run test-single-run -- --browsers PhantomJS'
+    
+    // archive karma test results (karma is configured to export junit xml files)
+    step([$class: 'JUnitResultArchiver', 
+          testResults: 'test-results/**/test-results.xml'])
+          
+}
+
+node 
+{
+    sh 'ls -la'
+    sh 'rm -rf *'
+    unstash 'everything'
+    sh 'ls -la'
+    //sh 'npm run test-single-run -- --browsers Chrome'
+}
+
+stage 'Browser Testing'
+{
+    parallel = 
+    chrome: 
+    {
+        runTests("Chrome")
+    }, 
+    firefox: 
+    {
+        runTests("Firefox")
+    }
+    safari: 
+    {
+        runTests("Safari")
+    }
+}
+
+def runTests(browser)
+{
+    node
+    {
+        sh 'rm -rf *'
+        unstash 'everything'
+        sh "npm run-test-single-run -- -- browsers ${browser}"
+        step([$class: 'JUnitResultsArchiver', testResults: 'test-results/**/test-results.xml'])
+    }
+}
+
+def notify(status){
+    emailext (
+      to: "wesmdemos@gmail.com",
+      subject: "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+      body: """<p>${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+        <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a></p>""",
+    )
+}
